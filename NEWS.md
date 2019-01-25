408c2e4b2fd1f2d9c0d5b577a5c86ed17ad5f71c
CorradoLanera <corrado.lanera@gmail.com>
2019-01-25 18:02:59 +0100

    data PROLOGUE imported

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,6 @@
+* PROLOGUE RCT data imported, merged and provided as a tibble
+  named `prologue`.
+
 # select 0.0.0.9000
 
 * Added basic support: git/GitHub, docs, CI, test, spellcheck, badges,
--- data-raw/import-data.R
+++ data-raw/import-data.R
@@ -16,4 +16,230 @@ knitr::opts_chunk$set(
 
 #+ package load
 library(tidyverse)
+library(readxl)
+library(janitor)
 library(here)
+
+
+#' ## PROLOGUE study data
+
+#' ### import
+
+#+ prologue import
+prologue_path <- here(
+    "data-raw",
+    "PROLOGUE_raw data for submission 20160506.xlsx"
+)
+
+prologue_sheets <-  prologue_path %>%
+    excel_sheets() %>%
+    # Sheet's names start with a number followed by a period.
+    str_subset("^[0-9]+\\. ")
+
+# set good names because they will be used by `map()`
+names(prologue_sheets) <- prologue_sheets %>%
+    str_replace_all(c(
+        "^[0-9]+\\. " = "",
+        " " = "_"
+    )) %>%
+    str_to_lower()
+
+
+prologue_raw_list <- map(prologue_sheets, ~{
+    read_xlsx(prologue_path, sheet = .x, na = c("", ".", " ")) %>%
+    clean_names()
+})
+
+walk(prologue_raw_list, glimpse)
+
+
+#' ### fix type
+
+#+ prologue fix
+prologue_raw_list[["allocation"]] <- prologue_raw_list[["allocation"]] %>%
+    mutate(
+        allocation = factor(allocation,
+            levels = c(1, 2),
+            labels = c("sitagliptin", "conventional")
+        ),
+        age = as.integer(age),
+        sex = factor(sex,
+            levels = c(1, 2),
+            labels = c("male", "female")
+        ),
+        dm_treatment = factor(dm_treatment,
+            levels = c(1, 2),
+            labels = c("non-pharmacological", "pharmacological")
+        ),
+        statin = statin == 1,
+        al_sbp = as.integer(al_sbp)
+    )
+
+glimpse(prologue_raw_list[["allocation"]])
+
+
+
+
+prologue_raw_list[["imt"]] <- prologue_raw_list[["imt"]] %>%
+    mutate(
+        allocation = factor(allocation,
+            levels = c(1, 2),
+            labels = c("sitagliptin", "conventional")
+        ),
+        age = as.integer(age),
+        sex = factor(sex,
+            levels = c(1, 2),
+            labels = c("male", "female")
+        ),
+        flag_fas = flag_fas == 0
+    )
+
+glimpse(prologue_raw_list[["imt"]])
+
+
+
+
+prologue_raw_list[["other_data"]] <- prologue_raw_list[["other_data"]] %>%
+    mutate_at(
+        vars(
+            history_hypertension:history_cerebrovascular_disorder_other,
+            history_myocardial_infarction:history_cardiovascular_disorder_other
+        ),
+        funs(. == 1)
+    ) %>%
+    mutate(
+        smoke_background = factor(smoke_background,
+            levels = c(0, 2, 1),
+            labels = c("no", "past", "yes"),
+            ordered = TRUE
+        ),
+        alcohol_background = if_else(alcohol_background == 9,
+            NA,
+            alcohol_background == 1
+        )
+    ) %>%
+    mutate_at(
+        vars(patient_condition_0m:patient_condition_24m),
+        funs(factor(., levels = c(1, 2), labels = c("good", "poor")))
+    ) %>%
+    mutate_at(
+        vars(sitagliptin_per_day_0m:sitagliptin_per_day_24m),
+        # values are 25, 50, 100, coded as 1, 2, 3.
+        funs(25 * 2^(. - 1))
+    ) %>%
+    mutate_at(vars(su_0m:ace_i_24m), funs(. == 1))
+
+glimpse(prologue_raw_list[["other_data"]])
+
+
+
+
+
+prologue_raw_list[["adjudicated_event"]] <- prologue_raw_list[["adjudicated_event"]] %>%
+    mutate_at(
+        vars(ev_sudden_death:ev_insulin),
+        funs(. == 1)
+    ) %>%
+    mutate(
+        ev_outcome = factor(ev_outcome,
+            levels = c(1, 2, 3, 4, 5),
+            labels = c(
+                "recovery", "remission", "recovery w/ after-effect",
+                "no-recovery", "death to the adverse event"
+            ),
+            ordered = TRUE
+        ),
+        ev_status = factor(ev_status,
+            levels = c(1, 2, 3),
+            labels = c(
+                "continuance", "dose reduction", "discontinuance"
+            ),
+            ordered = TRUE
+        )
+    )
+
+
+glimpse(prologue_raw_list[["adjudicated_event"]])
+
+
+
+
+
+prologue_raw_list[["adverse_effect"]] <- prologue_raw_list[["adverse_effect"]] %>%
+    mutate(
+        ae_classification = factor(ae_classification,
+            levels = c(1, 2),
+            labels = c("hypoglycemia", "others")
+        ),
+        ae_level = factor(ae_level,
+            levels = c(1, 2, 3),
+            labels = c("low", "moderate", "high"),
+            ordered = TRUE
+        ),
+        ae_severity_level_1 = factor(ae_severity_level_1,
+            levels = c(0, 1),
+            labels = c("non-severe", "severe"),
+            ordered = TRUE
+        ),
+        ae_severity_level_2 = factor(ae_severity_level_1,
+            levels = c(1, 2, 3, 4, 5),
+            labels = c(
+                "death", "critical", "hospitalization",
+                "permanent or extreme dysfunction", "heritable effect"
+            ),
+            ordered = TRUE
+        ),
+        ae_status = factor(ae_status,
+            levels = c(1, 2, 3),
+            labels = c(
+                "continuance", "dose reduction", "discontinuance"
+            ),
+            ordered = TRUE
+        ),
+        ae_status_other_1 = ae_status_other_1 == 1,
+        ae_outcome = factor(ae_outcome,
+            levels = c(1, 2, 3, 4, 5),
+            labels = c(
+                "recovery", "remission", "recovery w/ after-effect",
+                "no-recovery", "death to the adverse event"
+            ),
+            ordered = TRUE
+        ),
+        ae_effect_relation = factor(ae_effect_relation,
+            levels = c(0, 3, 2, 1),
+            labels = c(
+                "no correlation", "possible correlation",
+                "probable correlation", "correlation"
+            ),
+            ordered = TRUE
+        )
+    )
+
+glimpse(prologue_raw_list[["adverse_effect"]])
+
+
+
+
+#' ### merge
+
+#+ prologue join
+prologue <- reduce(prologue_raw_list, full_join, by = "id") %>%
+    group_by(id) %>%
+    mutate(
+        ae_quantity = n(),
+        ae_many = ae_quantity > 1
+    ) %>%
+    ungroup()
+message(paste(
+    "Some IDs are duplicated because they experienced more than one",
+    "adverse event (ae). For them there were one row each ae."
+))
+
+glimpse(prologue)
+
+
+#' ### save
+
+#+ save, error = TRUE
+usethis::use_data(prologue)
+
